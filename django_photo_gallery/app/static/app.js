2a2a3a02555fc7d4443676bdb7cf439958a41fab
anandkassu <anandkassu00@gmai.com>
2019-09-29 12:32:15 +0530

    album modified

--- django_photo_gallery/app/static/app.js
+++ django_photo_gallery/app/static/app.js
@@ -5,7 +5,7 @@
     $('.slider').slider();
 });
 
-// Initializes PhotoSwipe.
+
 var pswpInit = function (startsAtIndex) {
 
     if (!startsAtIndex) {
@@ -14,33 +14,14 @@ var pswpInit = function (startsAtIndex) {
 
     var pswpElement = document.querySelectorAll('.pswp')[0];
 
-    // commented the array bellow since the images array will be loaded from the server
-    // in variable called djangoAlbumImages.
-
-    // build items array
-    //var items = [
-    //    {
-    //        src: 'https://placekitten.com/600/400',
-    //        w: 600,
-    //        h: 400
-    //    },
-    //    {
-    //        src: 'https://placekitten.com/1200/900',
-    //        w: 1200,
-    //        h: 900
-    //    }
-    //];
-
-    // find is images are loaded from the server.
     if (window.djangoAlbumImages && window.djangoAlbumImages.length > 0) {
-        // define options (if needed)
+        
         var options = {
-            // optionName: 'option value'
-            // for example:
-            index: startsAtIndex // start at first slide
+            
+            index: startsAtIndex 
         };
 
-        // Initializes and opens PhotoSwipe
+        
         var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, window.djangoAlbumImages, options);
         gallery.init();
     }
--- django_photo_gallery/app/views.py
+++ django_photo_gallery/app/views.py
@@ -7,8 +7,8 @@ from django.views.generic import DetailView
 
 from app.models import Album, AlbumImage
 
-def home (request):
-    return render(request,'home.html')
+# def home (request):
+#     return render(request,'home.html')
     
 def gallery(request):
     list = Album.objects.filter(is_visible=True).order_by('-created')
